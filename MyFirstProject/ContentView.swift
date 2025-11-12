8bed048b123fb7e32a42903ccf0c760165cbd1d4
igor <iegurtov7@gmai.com>
2025-11-12 21:47:18 +0300

    Initial Commit

---
+++ MyFirstProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyFirstProject
+//
+//  Created by lulukun on 2025/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
