877f6fdc60d226698d834eca0d039e717b1005d0
igor <iegurtov7@gmai.com>
2025-11-14 00:51:29 +0300

    1

--- MyFirstProject/ContentView.swift
+++ MyFirstProject/ContentView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+        Text("Маргарита").font(.title)
             .padding()
-    }
+            Text("Четыре сыра").font(.title)
+                .padding()
+            Text("Пепeдрони").font(.title)
+                .padding()
+            Text("Гавайская").font(.title)
+                .padding()
+            Text("Барбекю").font(.title)
+                .padding()
+            Text("Грибная").font(.title)
+                .padding()
+            Text("Мексиканская").font(.title)
+                .padding()
+            Text("Секс на пляже").font(.title)
+                .padding()
+            Text("Охотнячая").font(.title)
+                .padding()
+            Text("").font(.title)
+                .padding()
+        
+        }.offset(x: 0, y: 0)
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(.trailing)
     }
 }
+}
+
